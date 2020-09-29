11dc4b5a632434c627de1588540219acc8c5ae09
Lars Norberg <gitkraken@larsnorbergofficial.com>
2020-09-29 10:47:08 +0200

    update!!!!! >:(

--- AzeriteUI/front-end/modules/menu-addon.lua
+++ AzeriteUI/front-end/modules/menu-addon.lua
@@ -3,6 +3,7 @@ local Core = Wheel("LibModule"):GetModule(ADDON)
 if (not Core) then 
 	return 
 end
+
 local Module = Core:NewModule("OptionsMenu", "HIGH", "LibMessage", "LibEvent", "LibDB", "LibFrame", "LibSound", "LibTooltip", "LibClientBuild")
 local MenuTable
 
